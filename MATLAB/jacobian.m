e6731fb370d274ed08e77aef7e3a2d31495971fd
Yirui Zhao <zhao106@illinois.edu>
2018-03-22 16:06:01 -0500

    fixed an indexing bug and made function slightly smarter

--- MATLAB/jacobian.m
+++ MATLAB/jacobian.m
@@ -1,8 +1,9 @@
-function J = jacobian(n, S, theta)
-% Finds the space Jacobian of an 6xn matrix
-% Takes in the number of joints of the robot, n
+function J = jacobian(S, theta)
+% Finds the space Jacobian of an 6xn matrix, where n is the number of joints
 % a matrix of n spatial screw axis S = [S1 S2 ... Sn]
 % and a matrix of n thetas
+    % find n based from columns of S
+    [m,n] = size(S);
     J = zeros(6,n);
     % Set first column of jacobian to S1
     J(:,1) = S(:,1);
@@ -12,7 +13,8 @@ function J = jacobian(n, S, theta)
     A_o = 1;
     % Build jacobian for rest of columns
     for N = 2:n
-        A_n = A_o * adjoint(expm(skew4(S(:,N))*theta(N)));
+        % Uses S and theta from 1 to n-1
+        A_n = A_o * adjoint(expm(skew4(S(:,N-1))*theta(N-1)));
         J(:,N) = A_n * S(:,N);
         A_o = A_n;
     end
