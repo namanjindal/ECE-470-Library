ab6ec58d1667434d035dac67afcd304b4147ba7f
Yirui Zhao <zhao106@illinois.edu>
2018-03-25 22:10:27 -0500

    added forward kinematics function

---
+++ MATLAB/find_fk.m
@@ -0,0 +1,13 @@
+function T = find_fk(S, theta, M)
+% returns a pose T
+% given a set of n spatial screw axis S = [S1 S2 .. Sn]
+% a matrix of n thetas, where theta can be 1xn or nx1
+% and the current pose, M
+    % n is number of joints
+    [m,n] = size(S);
+    T = 1;
+    for N = 1:n
+        T = T * expm(skew4(S(:,N))*theta(N));
+    end
+    T = T * M;
+end
