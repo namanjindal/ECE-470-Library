b37ac514a3deec5a3a95e4611b02f90389a64ed3
Yirui Zhao <zhao106@illinois.edu>
2018-03-25 22:11:09 -0500

    slightly editted function description

--- MATLAB/jacobian.m
+++ MATLAB/jacobian.m
@@ -1,7 +1,7 @@
 function J = jacobian(S, theta)
 % Finds the space Jacobian of an 6xn matrix, where n is the number of joints
 % a matrix of n spatial screw axis S = [S1 S2 ... Sn]
-% and a matrix of n thetas
+% and a matrix of n thetas. theta can be 1xn or nx1
     % find n based from columns of S
     [m,n] = size(S);
     J = zeros(6,n);
