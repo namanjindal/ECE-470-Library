f438a0278494af489477e1ac5d445c0066cd085a
Yirui Zhao <zhao106@illinois.edu>
2018-03-29 17:08:02 -0500

    improved readability

--- MATLAB/inv_t.m
+++ MATLAB/inv_t.m
@@ -1,12 +1,11 @@
-function [ t ] = inv_t( m )
+function [t] = inv_t(m)
+% Finds the inverse of a transformation matrix
+% Same as inv(), but faster
+    r = m(1:3,1:3);
+    p = m(1:3,4);
 
-% inverse of a transformation matrix
-r=[m(1,1) m(1,2) m(1,3);m(2,1) m(2,2) m(2,3);m(3,1) m(3,2) m(3,3)];
-p=[m(1,4);m(2,4);m(3,4)];
+    rn = r.';
+    pn = -r.'*p;
 
-rn = r.';
-pn = -r.'*p;
-
-t = [rn(1,1) rn(1,2) rn(1,3) pn(1);rn(2,1) rn(2,2) rn(2,3) pn(2);rn(3,1) rn(3,2) rn(3,3) pn(3);0 0 0 1];
-
+    t = [rn pn; 0 0 0 1];
 end
